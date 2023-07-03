7be104fc69721800903170778ba3de4b900877c4
Pedro Guimarães Alves Freitas <121042801+TyenW@users.noreply.github.com>
2023-07-02 23:40:10 -0300

    Add files via upload

---
+++ ti-1-ppl-cc-m2-20231-filtro-de-restaurantes-grupo-2-master/README.md
@@ -0,0 +1,20 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10875766&assignment_repo_type=AssignmentRepo)
+# Cheff Mustache
+Escreva um ou dois  parágrafo resumindo o objetivo do seu projeto.
+
+## Alunos integrantes da equipe
+
+-Clara Araújo Franco
+-Izabella Romano Bezerra Seabra
+-João Victor Romano Bezerra Seabra
+-João Victor Silluzio Ferreira
+-Pedro Guimarães Alves Freitas
+
+## Professores responsáveis
+
+-Ilo Amy Saldanha Rivero
+-Rafael Glater da Cruz Machado
+
+## Instruções de utilização
+
+Assim que a primeira versão do sistema estiver disponível, deverá complementar com as instruções de utilização. Descreva como instalar eventuais dependências e como executar a aplicação.
---
+++ ti-1-ppl-cc-m2-20231-filtro-de-restaurantes-grupo-2-master/codigo/Javascript/singup.js
@@ -0,0 +1,56 @@
+
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
+
+
+function salvarlog() {
+  var pessoas = localStorage.getItem("logins")
+
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+  var senha = document.getElementById("senha").value;
+  var confirma_senha = document.getElementById("confirma_senha").value;
+
+  if (senha == confirma_senha) {
+      alert("Conta criada com sucesso!!");
+
+      if (pessoas == null) {
+          pessoas = {
+              cadastros: [
+                  {
+                      nome: nome,
+                      email: email,
+                      senha: senha
+                  },
+              ]
+          }
+          localStorage.setItem("logins", JSON.stringify(pessoas))
+          window.location.href="trabalho-TI(2)/index.html"
+      }
+      else {
+          pessoas = JSON.parse(pessoas)
+          const novaConta = {
+              nome: nome,
+              email: email,
+              senha: senha
+          };
+          pessoas.cadastros.push(novaConta)
+          localStorage.setItem("logins", JSON.stringify(pessoas)) 
+          window.location.href="trabalho-TI(2)/index.html"
+      }
+      
+  }
+
+  else {
+
+      alert("As senhas não estão iguais!!");
+  }
+  href=":/trabalho-TI(2)/index.html"
+ 
+}
+function inicio()
+{
+ window.location.href='home.html'
+}
