6858f431ff0daec3d764290489910d81cae9f985
Pedro Guimarães Alves Freitas <121042801+TyenW@users.noreply.github.com>
2023-07-05 23:01:44 -0300

    Add files via upload

---
+++ codigo/Javascript/loguin.js
@@ -0,0 +1,36 @@
+
+function login() {
+  var username = document.getElementById("username").value
+  var password = document.getElementById("password").value
+  let dados = localStorage.getItem("logins")
+  var encontrado = false
+  var nomelogado 
+  dados = JSON.parse(dados)
+  dados.cadastros
+  for (i = 0; i < dados.cadastros.length; i++) {
+    if (username == dados.cadastros[i].nome) {
+      if (password == dados.cadastros[i].senha) {
+        nomelogado= dados.cadastros[i].nome
+        encontrado = true
+        break
+      }
+    }
+  }
+  if( encontrado == true ){
+    alert("Loguin Bem Sucedido!!")
+    alert(`Bem vindo ${nomelogado}, aproveite nosso site`)
+    window.location.href='home.html'
+  }
+  else {
+    alert("Senha Ou Usuario inesitente ou incorreto!!")
+  }
+}
+
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
+ function inicio()
+ {
+  window.location.href='home.html'
+ }
